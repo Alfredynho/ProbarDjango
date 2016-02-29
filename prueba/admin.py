7de475d9e8908b538a116df75a269ec91b905ae8
alfredo <callizayagutierrezalfredo@gmail.com>
2016-02-29 17:43:00 -0400

    male2

--- prueba/admin.py
+++ prueba/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from .models import Prueba
 # Register your models here.
-
+#esto es un cambio
+#comentario prueba
+#otro
 class mostrar(admin.ModelAdmin):
 	list_display = ['__str__','edad']
 	class Meta:
