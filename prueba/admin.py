0ee1968c25de9d9c84ae9c46cdcc6e2a27f00485
alfredo <callizayagutierrezalfredo@gmail.com>
2016-02-29 17:39:51 -0400

    Revert "Comentario"
    This reverts commit 3d80b3eb91ee95fcca231586c98e483148c79ee1.

--- prueba/admin.py
+++ prueba/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
 from .models import Prueba
 # Register your models here.
-#esto es un cambio
-#comentario prueba
+
 class mostrar(admin.ModelAdmin):
 	list_display = ['__str__','edad']
 	class Meta:
