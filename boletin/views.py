4b7943d96191bd9bf838d383fbe6068a51cce971
alfredo <callizayagutierrezalfredo@gmail.com>
2016-03-01 16:45:25 -0400

    Vistas,urls,templates

--- boletin/views.py
+++ boletin/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+#Aqui declaramos las vistas
+#se utiliza el request para solicitar algo
+#con el response se regresa o responde
+def inicio(request):
+	return render(request,"index.html",{})
