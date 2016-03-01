f77103991e3198f1089b61b553eea0757b7d746b
alfredo <callizayagutierrezalfredo@gmail.com>
2016-03-01 08:14:56 -0400

    nuevos cambios

--- prueba/admin.py
+++ prueba/admin.py
@@ -2,8 +2,7 @@ from django.contrib import admin
 from .models import Prueba
 # Register your models here.
 #registrando los modelos
-otros cambios
-practicando comandos git
+
 class mostrar(admin.ModelAdmin):
 	list_display = ['__str__','edad']
 	class Meta:
